31435b10acc03e2dfb5e7dd5623af1ea50cdb66a
ira <bakum21@ukr.net>
2016-05-31 17:24:31 +0300

    home - block img, services, industries, advantages, contacts

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,6 +1,17 @@
 
 jQuery(document).ready(function() {
 
-    
+
+
+
+    jQuery(window).load(function() {
+        ///blink
+        setInterval(function(){blink()}, 1000);
+        function blink() {
+            $("#header i span").fadeTo(300, 0).fadeTo(300, 1);
+        }
+    });
+
+
 
 });
