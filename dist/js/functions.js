e014d5ae5db06842e755969ae5b503d07013236a
ira <bakum21@ukr.net>
2016-05-31 18:33:24 +0300

    home block reviews

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -6,10 +6,30 @@ jQuery(document).ready(function() {
 
     jQuery(window).load(function() {
         ///blink
-        setInterval(function(){blink()}, 1000);
+        /*setInterval(function(){blink()}, 1000);
         function blink() {
             $("#header i span").fadeTo(300, 0).fadeTo(300, 1);
-        }
+        }*/
+
+
+        // Rewieves
+        $('.list_reviews').owlCarousel({
+            loop:true,
+            nav:true,
+            //autoplay:true,
+            //autoplayTimeout: 7000,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        })
     });
 
 
