a6c32ea9f2ce6d8c70e2a57327fd86e0519118ee
ira <bakum21@ukr.net>
2016-06-10 11:03:55 +0300

    menu link

--- src/js/functions.js
+++ src/js/functions.js
@@ -47,7 +47,22 @@ jQuery(document).ready(function() {
 
         });*/
 
-        
+        //scroll link menu
+        $(function() {
+            $('a[href*="#"]:not([href="#"])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                    if (target.length) {
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000);
+                        return false;
+                    }
+                }
+            });
+
+        });
        
     });
 
@@ -192,4 +207,9 @@ jQuery(document).ready(function() {
             });
         }*/
     });
+
+
+
+
+
 });
